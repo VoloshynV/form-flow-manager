dfa2cf697d2b35f860d8f67f23943d690ece0018
Volodymyr Voloshyn <riokin2011@gmail.com>
2023-09-25 22:44:17 +0200

    Handle active link

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,23 +1,22 @@
 "use client";
 
 import Link from "next/link";
-import { useParams } from "next/navigation";
+import { useParams, usePathname } from "next/navigation";
 import React from "react";
 
+import { cn } from "@/lib/utils";
+
 export const MainNav = () => {
+  const pathname = usePathname();
   const params = useParams();
 
   const routes = [
     {
-      path: "/",
-      name: "Home",
-    },
-    {
-      path: `/${params.companyId}/companies `,
+      path: `/${params.companyId}/companies`,
       name: "Companies",
     },
     {
-      path: `/${params.companyId}/submissions `,
+      path: `/${params.companyId}/submissions`,
       name: "Submissions",
     },
   ];
@@ -28,7 +27,10 @@ export const MainNav = () => {
         <Link
           key={route.path}
           href={route.path}
-          className="text-m hover:text-blue-500"
+          className={cn(
+            "text-m hover:text-blue-500",
+            pathname === route.path && "text-blue-300",
+          )}
         >
           {route.name}
         </Link>
